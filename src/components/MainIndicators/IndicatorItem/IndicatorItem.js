9102333cdafc4040a1a4c939708a9ad9af7ae607
Taras Burdeniuk <bur.taras@gmail.com>
2019-09-14 14:47:40 +0300

    edited MainIndicators

--- src/components/MainIndicators/IndicatorItem/IndicatorItem.js
+++ src/components/MainIndicators/IndicatorItem/IndicatorItem.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './IndicatorItem.css';
 
-const IndicatorItem = () => {
-    return <div className="IndicatorItem">IndicatorItem</div>;
+const IndicatorItem = ({ name, indicator, value, color, subName, percentValue }) => {
+    return (
+        <div className="IndicatorItem">
+            <div className="name-block">
+                <h4>{name}</h4>
+                <h4 className="indicator" style={{ background: `${color}` }}>
+                    {indicator}
+                </h4>
+            </div>
+            <div className="value-block">
+                <span className="value">{value}</span>
+                <div className="subName">
+                    <span className="subName-title">{subName}</span>
+                    <span style={{ color: `${color}`, fontSize: '1rem' }}>{percentValue}%</span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default IndicatorItem;
--- src/components/MainIndicators/MainIndicators.js
+++ src/components/MainIndicators/MainIndicators.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './MainIndicators.css';
+import { indicators } from './indicators';
 import IndicatorItem from './IndicatorItem/IndicatorItem';
 
 const MainIndicators = () => {
-    return (
-        <div className="MainIndicators">
-            <IndicatorItem />
-        </div>
-    );
+    const indicatorsItems = indicators.map(item => (
+        <IndicatorItem
+            name={item.name}
+            indicator={item.indicator}
+            value={item.value}
+            color={item.color}
+            subName={item.subName}
+            percentValue={item.percentValue}
+        />
+    ));
+
+    return <div className="MainIndicators">{indicatorsItems}</div>;
 };
 
 export default MainIndicators;
