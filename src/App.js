3fdca7c1ef25d3275a52834480c8aa7bb920ba94
Taras Burdeniuk <bur.taras@gmail.com>
2019-09-14 15:15:29 +0300

    added component Orders

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './reset.css';
 import './App.css';
 import MainIndicators from './components/MainIndicators/MainIndicators';
+import Orders from './components/Orders/Orders';
 
 const App = () => {
     return (
         <div className="App">
             <div className="container">
                 <MainIndicators />
+                <Orders />
             </div>
         </div>
     );
---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Orders.css';
+
+const Orders = () => {
+    return (
+        <div className="Orders">
+            <div className="name-block">
+                <h4>Orders</h4>
+                <div className="button-block">button block</div>
+            </div>
+        </div>
+    );
+};
+
+export default Orders;
